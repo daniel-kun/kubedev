b721fe3b0efb588311df4e4a6247e7a239f0f9d5
Daniel Albuschat <daniel.albuschat@gira.de>
2020-05-22 20:45:24 +0200

    Revived the testrange, by matching the call in run.sh to the new way that the kubedev cli is called

--- testrange/run.sh
+++ testrange/run.sh
@@ -1,3 +1,10 @@
-#rm -rf Tiltfile helm-chart .gitlab-ci.yml foo-deploy
+#!/bin/bash
+
+if [ $1 == "generate" ]
+then
+  echo -e ">>> Cleaning up before re-generating files <<<\n"
+  rm -rf Tiltfile helm-chart .gitlab-ci.yml foo-deploy
+fi
+
 export KUBEDEV_KUBECONFIG=default
-PYTHONPATH=/home/daniel/projects/kubedev/ python -m kubedev $* -c ../tests/kubedev.spec.json
+PYTHONPATH="`pwd`/../" python -m kubedev.cli $* -c ../tests/kubedev.spec.json
